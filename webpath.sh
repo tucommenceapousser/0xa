3f141877445a4edc942c021e1f9cb155b34c634f
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2023-12-15 04:28:25 +0100

    Update webpath.sh

--- webpath.sh
+++ webpath.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Fichier de sortie
+output_file="web_paths.txt"
+
 # Fonction pour rechercher les chemins web
 search_paths() {
-    read -p "Entrez le domaine ou le chemin à rechercher: " target
-
-    echo -e "\nRecherche des chemins web pour $target\n"
+    echo -e "\nRecherche des chemins web...\n"
 
     # Ajoutez ici vos commandes pour la recherche des chemins web
     paths=$(find / -type f \( -name "*.php" -o -name "*.html" -o -name "*.js" -o -name "*.css" -o -name "*.aspx" \) -exec dirname {} + 2>/dev/null)
@@ -12,11 +13,13 @@ search_paths() {
     # Affiche les résultats
     if [ -n "$paths" ]; then
         echo -e "\nChemins trouvés:\n$paths"
+        echo -e "$paths" > "$output_file"
+        echo -e "\nChemins enregistrés dans $output_file"
     else
-        echo -e "\nAucun chemin trouvé pour $target"
+        echo -e "\nAucun chemin trouvé."
     fi
 }
 
 # Interface colorée et interactive
-echo -e "\n\033[1;36mBienvenue dans le chercheur de chemins web interactif!\033[0m"
+echo -e "\n\033[1;36mBienvenue dans le chercheur de chemins web!\033[0m"
 search_paths
